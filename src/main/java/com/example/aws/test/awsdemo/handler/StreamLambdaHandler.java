cddd19a104f2e6a656cf5aff826d557c0d4467a0
James Carl Oreto <jamescarl533@gmail.com>
2026-01-01 12:56:46 +0800

    Update Lambda handler to use HttpApiV2ProxyRequest
    Refactored StreamLambdaHandler to use HttpApiV2ProxyRequest and the corresponding SpringBootLambdaContainerHandler method for HTTP API v2. Also updated hello endpoint response string for consistency.

--- src/main/java/com/example/aws/test/awsdemo/handler/StreamLambdaHandler.java
+++ src/main/java/com/example/aws/test/awsdemo/handler/StreamLambdaHandler.java
@@ -1,20 +1,21 @@
 package com.example.aws.test.awsdemo.handler;
 
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
-import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.example.aws.test.awsdemo.AwsdemoApplication;
 
-public class StreamLambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
+public class StreamLambdaHandler
+        implements RequestHandler<HttpApiV2ProxyRequest, AwsProxyResponse> {
 
-    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+    private static SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler;
 
     static {
         try {
-            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(
+            handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(
                     AwsdemoApplication.class);
         } catch (ContainerInitializationException e) {
             throw new RuntimeException("Could not initialize Spring Boot", e);
@@ -22,7 +23,7 @@ public class StreamLambdaHandler implements RequestHandler<AwsProxyRequest, AwsP
     }
 
     @Override
-    public AwsProxyResponse handleRequest(AwsProxyRequest input, Context context) {
+    public AwsProxyResponse handleRequest(HttpApiV2ProxyRequest input, Context context) {
         return handler.proxy(input, context);
     }
 }
