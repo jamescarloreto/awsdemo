f8eac64d2a195c678eb60d77311c64dbc3b215b7
James Carl Oreto <jamescarl533@gmail.com>
2026-01-06 23:23:08 +0800

    Update TestController.java

--- src/main/java/com/example/aws/test/awsdemo/controller/TestController.java
+++ src/main/java/com/example/aws/test/awsdemo/controller/TestController.java
@@ -2,10 +2,12 @@ package com.example.aws.test.awsdemo.controller;
 
 import java.util.ArrayList;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/list")
 public class TestController {
 
     @GetMapping("/list")
