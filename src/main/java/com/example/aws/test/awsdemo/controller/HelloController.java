49504b5250a987ab2ef40c71ee5d044ce078ca44
James Carl Oreto <jamescarl533@gmail.com>
2026-01-06 17:20:32 +0800

    Update HelloController.java

--- src/main/java/com/example/aws/test/awsdemo/controller/HelloController.java
+++ src/main/java/com/example/aws/test/awsdemo/controller/HelloController.java
@@ -1,14 +1,32 @@
 package com.example.aws.test.awsdemo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
         return "Happy New Year Everyone! <h1>🚀</h1>";
     }
+
+    @GetMapping("/list")
+    public ArrayList<String> getCity() {
+
+        return new ArrayList<String>() {
+            {
+                add("New York");
+                add("Los Angeles");
+                add("Chicago");
+                add("Houston");
+                add("Phoenix");
+            }
+        };
+    }
+    
 
 }
